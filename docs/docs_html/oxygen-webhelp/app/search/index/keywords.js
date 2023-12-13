703b0373ee862bde00855aee7264272ccd9866cf
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-12-13 11:23:03 +0530

    [Apps.]Restructured all documentation to use SDL generated Docs[MH3-88926]

---
+++ docs/docs_html/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,21 @@
+define(function() {var keywords=[{w:"Worldwide",p:["p0"]},{w:"Sales",p:["p0"]},{w:"and",p:["p0","p10"]},{w:"Service",p:["p0","p8"]},{w:"Quality",p:["p1"]},{w:"Management",p:["p1"]},{w:"System",p:["p1"]},{w:"Microchip",p:["p2","p5","p6","p11"]},{w:"Information",p:["p2"]},{w:"Legal",p:["p3"]},{w:"Notice",p:["p3"]},{w:"Trademarks",p:["p4"]},{w:"2",p:["p5"]},{w:"MPLAB\u00AE",p:["p5"]},{w:"Harmony",p:["p5","p7"]},{w:"3",p:["p5","p7","p9"]},{w:"Release",p:["p5"]},{w:"Notes",p:["p5"]},{w:"The",p:["p6"]},{w:"Website",p:["p6"]},{w:"1",p:["p7"]},{w:"USB",p:["p7"]},{w:"Dual",p:["p7"]},{w:"Role",p:["p7"]},{w:"application",p:["p7"]},{w:"examples",p:["p7"]},{w:"Product",p:["p8"]},{w:"Change",p:["p8"]},{w:"Notification",p:["p8"]},{w:"License",p:["p9"]},{w:"1.1",p:["p10"]},{w:"MSD",p:["p10"]},{w:"Host",p:["p10"]},{w:"HID",p:["p10"]},{w:"Device",p:["p10"]},{w:"Example",p:["p10"]},{w:"(host_msd_device_hid)",p:["p10"]},{w:"Devices",p:["p11"]},{w:"Code",p:["p11"]},{w:"Protection",p:["p11"]},{w:"Feature",p:["p11"]},{w:"Customer",p:["p12"]},{w:"Support",p:["p12"]},{w:"Context",p:["p13"]},{w:"Sensitive",p:["p13"]},{w:"Help",p:["p13"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3];
+ph["p1"]=[4, 5, 6];
+ph["p2"]=[7, 8];
+ph["p3"]=[9, 10];
+ph["p4"]=[11];
+ph["p5"]=[12, 7, 13, 14, 15, 16, 17];
+ph["p6"]=[18, 7, 19];
+ph["p7"]=[20, 14, 15, 21, 22, 23, 24, 25];
+ph["p8"]=[26, 27, 28, 3];
+ph["p9"]=[15, 29];
+ph["p10"]=[30, 31, 32, 2, 33, 34, 35, 36];
+ph["p12"]=[41, 42];
+ph["p11"]=[7, 37, 38, 39, 40];
+ph["p13"]=[43, 44, 45];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ docs/docs_html/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {4:2,12:2,1:2,7:-1,5:-1,11:2,6:2,10:7,2:-1,0:2,3:2,8:2,9:-1};
+});
