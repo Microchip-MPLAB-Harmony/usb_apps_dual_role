0f47fdcdf1bd03c8181c9b12c6d9f12fb5e3c6fa
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 16:18:05 +0530

    Regenerated demo for host_msd_device_hid_pic32mz_ef_curiosity_2_0
    MH3-107335

--- apps/host_msd_device_hid/firmware/src/config/pic32mz_ef_curiosity_2_0/interrupts.c
+++ apps/host_msd_device_hid/firmware/src/config/pic32mz_ef_curiosity_2_0/interrupts.c
@@ -77,17 +77,17 @@ void USB_DMA_Handler (void);
 // Section: System Interrupt Vector definitions
 // *****************************************************************************
 // *****************************************************************************
-void __ISR(_TIMER_1_VECTOR, ipl1SRS) TIMER_1_Handler (void)
+void __attribute__((used)) __ISR(_TIMER_1_VECTOR, ipl1SRS) TIMER_1_Handler (void)
 {
     TIMER_1_InterruptHandler();
 }
 
-void __ISR(_USB_VECTOR, ipl1SRS) USB_Handler (void)
+void __attribute__((used)) __ISR(_USB_VECTOR, ipl1SRS) USB_Handler (void)
 {
     DRV_USBHS_InterruptHandler();
 }
 
-void __ISR(_USB_DMA_VECTOR, ipl1SRS) USB_DMA_Handler (void)
+void __attribute__((used)) __ISR(_USB_DMA_VECTOR, ipl1SRS) USB_DMA_Handler (void)
 {
     DRV_USBHS_DMAInterruptHandler();
 }
